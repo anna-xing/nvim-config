2a1dcf98a6e82f7753ff963b494603a217faf838
Anna Xing <anna.xing.22@gmail.com>
2024-12-26 01:23:21 -0500

    working setup for django + ts react

---
+++ lua/config/options.lua
@@ -0,0 +1,12 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- Replace tabs with 2 spaces
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+-- Show line numbers
+vim.opt.relativenumber = false
