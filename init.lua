76ac716691d5cefe28eb570c96d7502d642a9de7
Anna Xing <anna.xing.22@gmail.com>
2025-01-06 12:27:46 -0500

    check if using vscode

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+if not vim.g.vscode then
+  -- Ordinary Neovim
+  -- bootstrap lazy.nvim, LazyVim and your plugins
+  require("config.lazy")
+end
